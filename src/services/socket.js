9af4c55c6b9b480a13312b8a823ef10c13fc1e6c
Trần Đình Thi <145179248+tdt2002qn@users.noreply.github.com>
2024-01-02 16:29:30 +0700

    Update socket 2.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 
 import io from 'socket.io-client';
 
-const socket = io('https://webdtbe.onrender.com');
+const socket = io('https://shopdientu.vercel.app');
 
 export const getSocket = () => {
   return socket;
