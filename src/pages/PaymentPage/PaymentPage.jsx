6d74ae707c5cbd074cf05c1f353e4c3af79a7ee5
Trần Đình Thi <145179248+tdt2002qn@users.noreply.github.com>
2024-01-04 09:12:56 +0700

    Update Paypal.jsx

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -262,7 +262,7 @@ const PaymentPage = () => {
                   <Lable>Chọn phương thức thanh toán</Lable>
                   <WrapperRadio onChange={handlePayment} value={payment}>
                     <Radio value="later_money"> Thanh toán tiền mặt khi nhận hàng</Radio>
-                    {/* <Radio value="paypal"> Thanh toán paypal</Radio> */}
+                     <Radio value="paypal"> Thanh toán paypal</Radio> 
                   </WrapperRadio>
                 </div>
               </WrapperInfo>
